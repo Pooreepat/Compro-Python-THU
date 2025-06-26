d7174a9e9411280ba475321a0aa6d2a7de10046a
Pooreepat <phaphutee@gmail.com>
2025-06-26 16:37:26 +0700

    First Commit With First class

---
+++ Lecture-01/tetprint.py
@@ -0,0 +1,8 @@
+# print('Hello, world!')
+# print('My Nname is Pooreepat Srichotikarn')
+# print('I am "Hungry" for knowledge') 
+# print('I am \'Hungry\' for knowledge') 
+# print('\ta\na\ta\ta\n\ta')
+print('''\ta
+a\ta\ta
+\ta''')
