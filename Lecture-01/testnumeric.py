7bedba98518d638175cbdd7dcfaa1dba2aa47422
Pooreepat <phaphutee@gmail.com>
2025-06-26 17:02:36 +0700

    End Lecher 1

---
+++ Lecture-01/testnumeric.py
@@ -0,0 +1,11 @@
+# from math import pi, e
+
+# print('%d' % 100)
+# print('%d' % 100.58)
+# print('%f' % 100.587)
+# print('%.2f' % 100.587)
+# print('%.6f' %pi)
+# print('My age is %d I have %.2f baht' % (25, 3500.58))
+# print('My age is '+ str(25) + ' I have ' + str(3500.50) + ' baht')
+
+print('5 + 4 = ',5+4)
